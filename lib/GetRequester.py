39ebacea0bcdc434d1b60a55060aac49551112d3
christine <christine.muriungi@student.moringaschool.com>
2024-01-27 00:32:22 +0300

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,26 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        #url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+        response = requests.get(self.url)
+        print(response.content)
+        return response.content 
 
     def load_json(self):
-        pass
+        response_content = self.get_response_body()
+
+        if response_content is not None:
+            try:
+                # Use json.loads to parse the JSON content
+                json_data = json.loads(response_content)
+                return json_data
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+                return None
+        else:
+            return None
+
+
+
+
